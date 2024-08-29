7993a1a548f5762205c297a839a5029890964838
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2024-08-29 09:51:28 +0100

    Update recommended cosma modules

--- scripts/flamingo/compile.sh
+++ scripts/flamingo/compile.sh
@@ -1,7 +1,11 @@
+#!/bin/bash -l
+
+# Compiles rockstar-swift for use in COSMA
+cd ../../
+
 module purge
-module load intel_comp/2019
+module load intel_comp/2024.2.0 compiler-rt tbb compiler mpi
 module load hdf5
 
 make with_hdf5
 make parents
-
