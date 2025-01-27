0c579197b6f5cc4016aa19e65aaf7caabb699877
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2025-01-27 16:22:01 +0000

    Update compilation modules

--- scripts/flamingo/compile.sh
+++ scripts/flamingo/compile.sh
@@ -3,9 +3,6 @@
 # Compiles rockstar-swift for use in COSMA
 cd ../../
 
-module purge
-module load intel_comp/2024.2.0 compiler-rt tbb compiler mpi
-module load hdf5
-
+module load gnu_comp/13.1.0 hdf5/1.12.2 openmpi/4.1.4
 make with_hdf5
 make parents
